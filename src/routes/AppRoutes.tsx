10bfcdcf75ecc1119c7b5999d77f1377aa950b70
seif <mohamedahmedseifofficial@gmail.com>
2025-08-11 20:44:25 +0300

    Installing and configureing shadcn with vite.v7 + tailwind.v3, applayout and outlet, home page, starting navbar component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,16 @@
-import { Routes , Route, BrowserRouter as Router} from 'react-router-dom'
+import AppLayout from "@/components/AppLayout";
+import Home from "@/pages/Home";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 export default function AppRoutes() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<div>Home</div>} />
+  return (
+    <Router>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<div>About</div>} />
           <Route path="/contact" element={<div>Contact</div>} />
-        </Routes>
-      </Router>
-  )
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
