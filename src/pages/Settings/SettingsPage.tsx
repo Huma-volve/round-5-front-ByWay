cc5546f38b65b679052426774d4a1ae9b494b9cc
Mustafa Ihab <mustafaihab9@gmail.com>
2025-08-12 15:33:16 +0300

    completed the UI design for Favourite, payments , settings

---
+++ src/pages/Settings/SettingsPage.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from "react-i18next";
+import Breadcrumb from "../../components/common/Breadcrumb";
+import { useBreadcrumb } from "../../hooks/useBreadcrumb";
+import arrow from "../../assets/images/icons/setting-arrow.png";
+import { Link } from "react-router-dom";
+
+export default function SettingsPage() {
+  const { t } = useTranslation();
+  const { getAutoBreadcrumb } = useBreadcrumb();
+
+  return (
+    <div className="bg-background">
+      {/* the stack of pathes */}
+      <div className="m-4 mt-8">
+        <Breadcrumb items={getAutoBreadcrumb()} className="mb-6 mt-5" />
+      </div>
+      {/* payment method */}
+      <div className="mt-8 ml-4 md:ml-24 flex justify-between w-[90%] md:w-[50%] items-center">
+        <h4 className="text-base font-semibold md:text-lg">{t("Payment Methods")}</h4>
+        <Link to="paymethod">
+        <img src={arrow} alt="drop down" className="w-3 h-3" />
+        </Link>
+      </div>
+      {/* payment history */}
+      <div className="mt-8 ml-4 md:ml-24 flex justify-between w-[90%] md:w-[50%] items-center">
+        <h4 className="text-base font-semibold md:text-lg">{t("Payment History")}</h4>
+        <Link to="payhistory">
+        <img src={arrow} alt="drop down" className="w-3 h-3" />
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,32 @@
 import { Routes , Route, BrowserRouter as Router, Link} from 'react-router-dom'
 import InstructorReviews from '../pages/instructor/InstructorReviews'
+import Favourites from '../pages/Favourites/Favourites'
+import NotificationPage from "../pages/Notifications/NotificationPage"; 
+import SettingsPage from '../pages/Settings/SettingsPage';
+import PaymethodPage from '../pages/Payments/PaymethodPage';
+import PayHistoryPage from '../pages/Payments/PayHistoryPage';
 export default function AppRoutes() {
     return (
       <Router>
         <Routes>
-                <Route path="/" element={<div>Home
+                <Route path="/" element={
+                  <div className='flex gap-8'>Home
                     <Link to="/instructor/reviews">Instructor Reviews</Link>
-          </div>} />
+                    <Link to="/favourites">Favourites</Link>
+                    <Link to="/notifications">Notifications</Link>
+                    <Link to="/settings">Settings</Link>
+                  </div>
+        } />
                 <Route path='/instructor' >
                     <Route path='reviews' element={<InstructorReviews />} />
                 </Route>
+                {/* it must add as a child for BrowserCourses */}
+                <Route path='/favourites' element={<Favourites />} />
+                <Route path='/notifications' element={<NotificationPage />} />
+                <Route path='/settings' element={<SettingsPage />} />
+                <Route path='/settings/paymethod' element={<PaymethodPage />} />
+                <Route path='/settings/payhistory' element={<PayHistoryPage />} />
+                {/*End of children for BrowserCourse */}
         </Routes>
       </Router>
   )
