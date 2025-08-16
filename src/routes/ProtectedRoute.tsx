f2de0dc39b1ba303cf4c879f04c1d7bbba8f1168
Mustafa Ihab <mustafaihab9@gmail.com>
2025-08-17 00:12:53 +0300

    commiting

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,5 +2,5 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }:any ) {
   const isAuthenticated = !!localStorage.getItem("user"); // example auth check
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
+  return isAuthenticated ? children : <Navigate to="/signin" replace />;
 }
