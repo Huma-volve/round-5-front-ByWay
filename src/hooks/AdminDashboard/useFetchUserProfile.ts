9420a76f8e7577ec28ef6ebb9531f833508c44c5
Mustafa Ihab <mustafaihab9@gmail.com>
2025-08-20 15:55:21 +0300

    editing the dropmenu

--- src/hooks/AdminDashboard/useFetchUserProfile.ts
+++ src/hooks/AdminDashboard/useFetchUserProfile.ts
@@ -5,7 +5,14 @@ import { useQuery } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
 
 export function useFetchUserProfileDashboard(id: number) {
+<<<<<<< HEAD
   return useQuery<UserProfileDashboard, AxiosError>({
+=======
+  const { data, error, isLoading, isError } = useQuery<
+    UserProfileDashboard,
+    AxiosError
+  >({
+>>>>>>> 25ed4a96662e52d7b59a8d0f25d388dbef40d9bb
     queryKey: ["userProfileDashboard", id],
     queryFn: () => fetchUsersDashboardProfile(id),
     enabled: !!id,
