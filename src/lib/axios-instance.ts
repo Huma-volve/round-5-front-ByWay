f888c47414740de8e308a4b9a3ee620d070291e0
mohamed <egyptgoschool@gmail.com>
2025-08-22 20:46:44 +0300

    fix: update axios baseURL to use HTTPS for improved security

--- src/lib/axios-instance.ts
+++ src/lib/axios-instance.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://round5-byway.huma-volve.com/api/",
+  baseURL: "https://round5-byway.huma-volve.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
