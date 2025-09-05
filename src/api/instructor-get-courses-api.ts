017870665835d09faa969e040d233b6a08cd0bb4
Mohamed Nasr <mnasr0868@gmail.com>
2025-09-05 18:57:15 +0300

    manage my courses for instructor pagination loading no courses and integrate with api

---
+++ src/api/instructor-get-courses-api.ts
@@ -0,0 +1,16 @@
+import axiosInstance from "@/lib/axios-instance";
+import type { AxiosError } from "axios";
+import { toast } from "react-toastify";
+
+export function fetchInstructorCourses(pageNumber?: number) {
+  try {
+    const response = axiosInstance.get(
+      `instructor/course-management/courses?page=${pageNumber}`
+    );
+    return response;
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message?: string }>;
+    toast.error(axiosError.response?.data.message || "Failed to fetch courses");
+    throw error;
+  }
+}
